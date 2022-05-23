e0de42faba4308c1e6e1162fd285609747a0befa
Victoria Denisse Reynoso <victoriareynoso88@gmail.com>
2022-05-23 02:07:33 -0300

    Migrations, Models y Seeders de Sectores, hechos.

---
+++ src/db/migrations/20220523002345-create-area.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Areas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      maxCapacity: {
+        type: Sequelize.INTEGER
+      },
+      currentOcupation: {
+        type: Sequelize.INTEGER
+      },
+      openingHour: {
+        type: Sequelize.STRING
+      },
+      closingHour: {
+        type: Sequelize.STRING
+      },
+      state: {
+        type: Sequelize.STRING
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Areas');
+  }
+};
