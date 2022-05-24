14bfaeae937fbb373486a36907b62193cbd4512f
Victoria Denisse Reynoso <victoriareynoso88@gmail.com>
2022-05-24 18:12:55 -0300

    Creación Model Guests

--- src/db/migrations/20220524204415-CreateGuestsTable.js
+++ src/db/migrations/20220524204415-CreateGuestsTable.js
@@ -2,20 +2,53 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('guests', { 
+      id:{
+      type: Sequelize.DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      initialAutoIncrement: 1,
+      },
+      id_room:{
+      type: Sequelize.DataTypes.INTEGER,
+      allowNull: false,
+      },
+      identificationNumber:{
+      type: Sequelize.DataTypes.INTEGER,
+      allowNull: false,
+      unique: true
+      },
+      firstName:{
+      type: Sequelize.DataTypes.STRING(30),
+      allowNull: false,
+      },
+      lastName:{
+      type: Sequelize.DataTypes.STRING(30),
+      allowNull: false,
+      },
+      age:{
+      type: Sequelize.DataTypes.INTEGER,
+      allowNull: false,
+      },
+      gender:{
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
+      },
+      createdAt:{
+      type: Sequelize.DataTypes.DATE,
+      defaultValue: Sequelize.DataTypes.NOW
+      },
+      updatedAt:{
+      type: Sequelize.DataTypes.DATE,
+      defaultValue: Sequelize.DataTypes.NOW
+      },
+        
+        
+    });
+
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('guests');
   }
 };
