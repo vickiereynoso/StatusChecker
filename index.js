4cd705baf7ba3b3590f6118b63f8718adeb946aa
Victoria Denisse Reynoso <victoriareynoso88@gmail.com>
2022-05-24 17:19:28 -0300

    Creación rutas/endpoints

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 //Definir rutas o endpoints, con sus parámetros.
+const express = require(express)
+const app = express()
+const {Request} = require('./src/db/models')
+
+app.get('/requests', async function(req,res){
+	
+	let data = await Request.findAll()
+    res.send(data)
+    })
+app.listen(8000) 
 
