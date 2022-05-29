8ade95fe9fa19a540647ad923befb1200965a58e
Victoria Denisse Reynoso <victoriareynoso88@gmail.com>
2022-05-29 19:29:53 -0300

    Armado rutas

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 //Definir rutas o endpoints, con sus parámetros.
+const express = require('express')
+const app = express()
+const {Rating} = require('./src/db/models')
+
+app.get('/ratings', async function(req,res){
+	
+	let data = await Rating.findAll()
+    res.send(data)
+    })
+
+app.get('/ratings/:id', async function(req,res){
+    let data = await Rating.findByPk(req.params.id)
+    res.send(data)
+    })
+    
+app.listen(8000)
 
