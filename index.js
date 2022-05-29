96fef756416e51b5fa360d84d2c27df6ec2d3846
Victoria Denisse Reynoso <victoriareynoso88@gmail.com>
2022-05-29 05:32:00 -0300

    Creación rutas en index.js.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 //Definir rutas o endpoints, con sus parámetros.
+const express = require('express')
+const app = express()
+const {Request} = require('./src/db/models')
+
+//Me traigo todas las requests.
+app.get('/requests', async function(req,res){
+	let data = await Request.findAll()
+    res.send(data)
+    })
+
+//Me traigo de a una.
+app.get('/requests/:id', async function(req,res){
+    let data = await Request.findByPk(req.params.id)
+    res.send(data)
+    })
+    
+app.listen(8000)
 
