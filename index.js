dbc16013b0402728abdfe1f5314c2ff316db059a
Jatzso <91919512+Jatzso@users.noreply.github.com>
2022-06-14 20:34:12 -0300

    TestCalificaciones

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+//SANTI
 //Definir rutas o endpoints, con sus parámetros.
+const { default: axios } = require('axios')
 const express = require('express')
 const app = express()
 const {Rating, Guest, Area, Request} = require('./src/db/models')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 
+//**************** */
+//SANTI
+app.get('/requests', async function(req, res){
+    console.log(req.body)
+    let data = await Request.findOne({
+        where : {
+            id_guest: req.params.id_guest
+        }  
+    }).then(data => {
+        console.log(data)
+        res.status(201).json({})
+    }).catch(err => {
+        console.log(err)
+        res.status(422).json(err)
+    })
+})
+
+
+//GET RATINGS
 app.get('/ratings', async function(req,res){
 	
    
@@ -11,10 +34,44 @@ app.get('/ratings', async function(req,res){
     res.send(data)
     })
 
+//GET RATINGS/ID
 app.get('/ratings/:id', async function(req,res){
     let data = await Rating.findByPk(req.params.id)
     res.send(data)
     })
+
+//POST RATINGS
+//SANTI
+//Creo en la bbdd una calificacion para testear:
+app.post('/ratings', async function(req, res){
+
+    let count = await Rating.count({
+        where : {
+            id: req.body.id
+        }
+    })  //Busco la cantidad que existe.
+    if (count > 0) {
+    return res.status(422).json({message:'RATING_EXISTS'})    //Si existe mando mensaje de error.
+    }
+    Rating.create({
+        /** id_area: DataTypes.INTEGER,
+    id_request: DataTypes.INTEGER,
+    score: DataTypes.INTEGER,
+    review: DataTypes.STRING */
+        id: req.body.id,
+        id_area: req.body.id_area,
+        id_request: req.body.id_request,
+        score: req.body.score,
+        review: req.body.review,
+    
+/* createdAt: req.body.createdAt,
+        updatedAt: req.body.updatedAt */
+    }).then(data => {
+        res.status(201).json({})
+    }).catch(err => {
+        res.status(422).json(err)
+    })
+})
     
 //app.listen(8000)
 
@@ -60,6 +117,8 @@ app.get('/requests/:id', async function(req,res){
 	let data = await Request.findByPk(req.params.id)
     res.send(data)
 })
+
+
 
 
 
--- test/calificacionesTest.js
+++ test/calificacionesTest.js
@@ -1,7 +1,7 @@
-calificaciontest.js
+//calificaciontest.js
 //REGLA DE NEGOCIO: Que la calificación sea entre 1 a 5.
-
-/*Necesito:
+/*
+Necesito:
 1. Testear que el huésped exista. Esperado: que exista.
 2. Testear que el sector exista.
 3. Testear que el huesped haya concurrido aunque sea 1 vez a dicho sector:
@@ -10,14 +10,144 @@ calificaciontest.js
    Testear que la calificación sea menor a 11.
 */
 
-/* const axios = require('axios');
+const axios = require('axios');
 const chai = require('chai');
 const chaiFetch = require('chai-fetch');
 chai.use(chaiFetch);  //chai es como un motor que ejecuta otras cosas, y acá le pasamos/instalamos ese plugin internamente para que lo pueda usar.
 
 const { assert } = chai; 
 
+//1. Testear que el huésped exista. Esperado: que exista.
+describe("Huesped existente", () => {
+    it('el status de respuesta debe ser 200', (done)=>{
+        axios({
+            method: 'get',
+            url: 'http://localhost:7000/guests/:id',
+            data: {
+                id: 1,
+            }
+        }).then(response=>{
+            console.log(response.status)
+            console.log(response.data)
+            assert.equal(response.status, 200)
+            done()
+        }).catch(err => {
+            console.log(err.message)
+        })
+    })
+})
 
+//2. Testear que el sector exista.
+describe("Area existente", () => {
+    it('el status de respuesta debe ser 200', (done)=>{
+        axios({
+            method: 'get',
+            url: 'http://localhost:7000/areas/:id',
+            data: {
+                id: 1,
+            }
+        }).then(response=>{
+            console.log(response.status)
+            console.log(response.data.id)
+            assert.equal(response.status, 200)
+            done()
+        }).catch(err => {
+            console.log(err.message)
+        })
+    })
+})
+
+//3. Testear que el huesped haya concurrido aunque sea 1 vez a dicho sector:
+ //(lo buscás en la tabla de registro de asistencias).
+
+
+//TENGO QUE CAMBIAR LOS DATOS PARA CREAR UNA NUEVA CALIFICACIÓN. 
+//DE LO CONTRARIO TIRA ERROR PORQUE CHOCA CON UN DATO EXISTENTE
+// describe("Creando calificacion", () => {
+//     it('el status de respuesta debe ser 200', (done)=>{
+//         axios({
+//             method: 'post',
+//             url: 'http://localhost:7000/ratings',
+//             data: {
+//                 id: 36,
+//                 id_area: 4,
+//                 id_request: 60,
+//                 score: 5,
+//                 review: 'Increible',
+//             }
+//         }).then(response=>{
+//             console.log(response.status)
+//             assert.equal(response.status, 201)
+//             done()
+//         }).catch(err => {
+//             console.log(err.message)
+//         })
+//     })
+// })
+
+
+
+// describe("Testenado que un huesped haya asistido en una area", () => {
+//     it('el status debe ser 201', (done)=>{
+//         axios({
+//             method: 'get',
+//             url: 'http://localhost:7000/requests/:id_guest',
+//             data: {
+//                 id_guest: 29421209
+//             }
+//         }).then(response=>{
+//             //console.log(response.data.id)
+//             assert.equal(response.status, 201)
+//             //assert.isBelow(response.data.score, 11, "el score es menor a 10")
+//             done()
+//         })
+//     })
+// })
+
+describe("Testeando calificación menor a 11", () => {
+    it('el score debe ser menor a 11', (done)=>{
+        axios({
+            method: 'get',
+            url: 'http://localhost:7000/ratings/1',
+        }).then(response=>{
+            assert.isBelow(response.data.score, 11, "el score es menor a 10")
+            done()
+        })
+    })
+})
+
+describe("Testeando calificación mayor a 0", () => {
+    it('el status de respuesta debe ser 200', (done)=>{
+        axios({
+            method: 'get',
+            url: 'http://localhost:7000/ratings/1',
+        }).then(response=>{
+            assert.isAbove(response.data.score, 0)
+            console.log(response.data)
+            done()
+        })
+    })
+})
+
+// describe("Creando calificacion", () => {
+//     it('el status de respuesta debe ser 200', (done)=>{
+//         axios({
+//             method: 'get',
+//             url: 'http://localhost:7000/ratings/1',
+//         }).then(response=>{
+//             //console.log(response.data.id)
+//             console.log(response.status)
+//             assert.equal(response.data.id_request, 6, "COINCIDEN")
+//             //assert.isBelow(response.data.score, 11, "el score es menor a 10")
+//             done()
+//         }).catch(err => {
+//             console.log(err.message)
+//         })
+//     })
+// })
+
+
+/*
 describe('Guest exists', () =>{
     it ('return code 201 if guest exists', (done) => {
 
@@ -66,4 +196,5 @@ describe('The guest checked in on area at least once.', () =>{
             done()
         })
     })
-}) */
+}) 
+*/
